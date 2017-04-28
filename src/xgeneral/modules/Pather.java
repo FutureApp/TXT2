2968faae253f5923e45a2ca63e1474d4d356c8bc
Michael Czaja <MichaelCzaja@fico.com>
2017-04-28 11:43:14 +0200

    patherToResult File

--- src/xgeneral/modules/Pather.java
+++ src/xgeneral/modules/Pather.java
@@ -4,5 +4,6 @@ public class Pather {
 
 	public static final String toLib="lib/";
 	public static final String toLog="logs/";
+	public static final String toResult="./";
 	public static final String toTestTask="task/00TempTask";
 }
