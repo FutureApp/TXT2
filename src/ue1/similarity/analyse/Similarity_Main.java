3e65981b44ae63174d1ad2bd998c238107a77916
Michael Czaja <michael-czaja-arbeit@hotmail.de>
2017-05-23 02:27:03 +0200

    Start Task1.03

---
+++ src/ue1/similarity/analyse/Similarity_Main.java
@@ -0,0 +1,45 @@
+package ue1.similarity.analyse;
+
+import xgeneral.modules.Encoding;
+import xgeneral.modules.SystemMessage;
+
+public class Similarity_Main {
+
+	static String[] arg;
+	static String encoding = Encoding.getDefaultEncoding();
+
+	/**
+	 * Entry-point of application.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		arg = args;
+		validateAmountOfGivenInput();
+	}
+
+	/**
+	 * Checks if the amount of the given input matches the requirements. If okay
+	 * then pass else print usage() and terminate program with exit-code 2.
+	 */
+	public static void validateAmountOfGivenInput() {
+		if (arg.length < 3) {
+			SystemMessage.eMessage("More input is needed");
+			System.out.println();
+			for (int i = 0; i < arg.length; i++) {
+				System.out.printf("Argument %d: %s", i, arg[i]);
+				System.out.println();
+			}
+			usage();
+			System.exit(2);
+		}
+	}
+
+	/**
+	 * Prints usage
+	 */
+	private static void usage() {
+		System.out.println("---------- Usage ----------");
+		System.out.println("java -jar <name of jar>.jar ");
+	}
+}
