887283baacc54895d6358f0b221e8707e7236274
Michael Czaja <michael-czaja-arbeit@hotmail.de>
2017-05-23 08:12:36 +0200

    clean and test start

--- src/ue1/similarity/analyse/Similarity_Main.java
+++ src/ue1/similarity/analyse/Similarity_Main.java
@@ -1,10 +1,12 @@
 package ue1.similarity.analyse;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 import xgeneral.modules.Encoding;
 import xgeneral.modules.SystemMessage;
+import xgeneral.modules.Writer;
 
 public class Similarity_Main {
 
@@ -22,18 +24,42 @@ public class Similarity_Main {
 		check(arg);
 
 		String dirInput = "./temp/res/src";
-		String pathMatrixExport = "./temp/matrixExport.txt";
-		String pathListExport = "./temp/listExport.txt";
+		String pathMatrixExport = "./result/matrixExport.txt";
+		String pathListExport = "./result/listExport.txt";
 		SimilarityAnalyzer programm = new SimilarityAnalyzer(dirInput, pathMatrixExport, pathListExport);
 		ArrayList<DocumentSignatureGramm> allSigis = programm.generateDocumentSignature();
 		programm.saveSignatures(allSigis);
 		HashMap<String, DocumentSignatureGramm> generateHashMapOfSigis = programm.generateHashMapOfSigis(allSigis);
 		MyMatrix simiMatrix = programm.generateSimilarityMatrix(allSigis);
-		
+
 		System.out.println(simiMatrix.matrixToString());
 		programm.checkSimiliartiy(simiMatrix, generateHashMapOfSigis);
-		System.out.println("Resulst");
+		ArrayList<SSDTupele> detHighestSimiliForDocs = programm.detHighestSimiliForDocs(simiMatrix);
+		SSDList highestSimiliList = new SSDList(detHighestSimiliForDocs, "doc 1", "highest match with doc2",
+				"Similarity");
+		System.out.println(highestSimiliList.toString());
+		System.out.println("Result");
 		System.out.println(simiMatrix.matrixToString());
+
+		System.out.println("Exporting results -- starting");
+		File exportMatrixFile = new File(pathMatrixExport);
+		File exportListFile = new File(pathListExport);
+
+		Writer.delAndWrite(exportMatrixFile, simiMatrix.matrixToString());
+		Writer.delAndWrite(exportListFile, highestSimiliList.toString());
+
+		System.out.println("Exporting results -- finsihed");
+		System.out.println();
+		System.out.println("Location of matrix -> " + exportMatrixFile.getAbsolutePath());
+		System.out.println("Location of list -> " + exportListFile.getAbsolutePath());
+		
+		PrintByeBye();
+
+	}
+
+	private static void PrintByeBye() {
+		// TODO Auto-generated method stub
+		
 	}
 
 	private static void check(String[] arg2) {
