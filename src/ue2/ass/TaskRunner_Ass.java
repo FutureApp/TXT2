82ab237f809570f44ff7457f7af03293217b686a
Michael Czaja <michael-czaja-arbeit@hotmail.de>
2017-06-22 13:52:12 +0200

    Para

--- src/ue2/ass/TaskRunner_Ass.java
+++ src/ue2/ass/TaskRunner_Ass.java
@@ -9,7 +9,7 @@ import ue1.kSkipN.newpack.TeiP5;
 import xgeneral.modules.Encoding;
 import xgeneral.modules.SystemMessage;
 
-public class TaskRunner_Template {
+public class TaskRunner_Ass {
 
 	static String[] arg;
 	static String encoding = Encoding.getDefaultEncoding();
@@ -22,18 +22,33 @@ public class TaskRunner_Template {
 	public static void main(String[] args) {
 		arg = args;
 		validateAmountOfGivenInput();
-		
+
+		// Init
 		String fileLoc = "C:/Users/admin/Desktop/temp/Franz_Kafka_In_der_Strafkolonie/alo.tei";
+		String outPutLoc = "./output.atff";
 		String firstClusteringTag = "p";
-		String secondClusteringTag= "w";
+		String secondClusteringTag = "w";
 		int condition = 2;
 
+		// From user
+		firstClusteringTag = args[0];
+		secondClusteringTag = args[1];
+		condition = myParseCase(args[2]);
+		fileLoc = args[3];
+		outPutLoc = args[4];
+
+		// Start of pre -processing
 		UE_MainProcess main = new UE_MainProcess();
 		TeiP5 readFile = main.readFile(fileLoc);
 
-		ArrayList<ArrayList<Node>> wordsInPara = main.abstractsNeededInfos(readFile, "p", "w");
+		// Processing
+		ArrayList<ArrayList<Node>> wordsInPara = main.abstractsNeededInfos(readFile, firstClusteringTag,
+				secondClusteringTag);
+
 		ArrayList<ArrayList<String>> entrysOfParagraphs = main.generateEntry(wordsInPara, condition);
 		HashMap<String, Integer> generateEntryHash = main.generateEntryHash(entrysOfParagraphs);
+
+		// Export
 		ARFF_Exporter arffDoc = new ARFF_Exporter(generateEntryHash);
 		ArrayList<String> trashParagraphs = new ArrayList<>();
 		for (int i = 0; i < entrysOfParagraphs.size(); i++) {
@@ -45,14 +60,38 @@ public class TaskRunner_Template {
 				arffDoc.attachDataToList(paragraph);
 			}
 		}
-		arffDoc.exportMeToFile("./arffDoc");
+		// The real export.
+		arffDoc.exportMeToFile(outPutLoc);
 
+		// End-Mess
 		System.out.println(main.uniqueWords);
 		System.out.println("Paragraphs " + wordsInPara.size());
 		System.out.println("Paragraphs words " + entrysOfParagraphs.size());
 		System.out.println("UniqureWord:" + main.uniqueWords);
 		System.out.println(trashParagraphs);
 		System.out.println(trashParagraphs.size());
+	}
+
+	private static int myParseCase(String caseNum) {
+		int result = 0;
+		switch (caseNum) {
+		case "0":
+			result = 0;
+			break;
+		case "1":
+			result = 1;
+			break;
+		case "2":
+			result = 2;
+			break;
+
+		default:
+			SystemMessage.wMessage("Case unknown. Use default -- 0");
+			result = 0;
+			break;
+		}
+
+		return result;
 	}
 
 	/**
